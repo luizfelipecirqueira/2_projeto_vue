bab4c9e8a8e3c0b41556b4d19c425e9b271e7983
unknown <lfcs18ts@gmail.com>
2022-08-08 21:18:50 -0300

    Trabalhando componentes com vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+export default {
+  name: "App",
+  components: { PrimeiroComponente },
+};
+</script>
