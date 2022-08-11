0ce03a5260333ffc000f0bba3c21e8c37d98e825
unknown <lfcs18ts@gmail.com>
2022-08-11 11:37:54 -0300

    Lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <PrimeiroComponente />
+    <LifeCycle />
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+
 export default {
   name: "App",
-  components: { PrimeiroComponente },
+  components: { PrimeiroComponente, LifeCycle },
 };
 </script>
